0e7bb5cb14709f9a82cdd7d0641b9d88df52cb50
Guilherme Lima <guilherme.lima@alura.com>
2022-01-05 17:13:11 -0300

    search por tags

--- restaurantes/models.py
+++ restaurantes/models.py
@@ -9,60 +9,25 @@ class Restaurante(models.Model):
 
 class Prato(models.Model):
     TAGS = (
-        ('Aves', 'Aves'),
-        ('Arroz','Arroz'),
         ('Doces', 'Doces'),
-        ('Bolos', 'Bolos'),
         ('Caldos', 'Caldos'),
-        ('Carne', 'Carne'),
-        ('Alemã', 'Alemã'),
         ('Americana', 'Americana'),
-        ('Árabe', 'Árabe'),
-        ('Argentina', 'Argentina'),
-        ('Australiana', 'Australiana'),
         ('Baiana', 'Baiana'),
-        ('Capixaba', 'Capixaba'),
-        ('Chinesa', 'Chinesa'),
-        ('Colombiana', 'Colombiana'),
         ('Coreana', 'Coreana'),
-        ('Cubana', 'Cubana'),
         ('Espanhola','Espanhola' ),
         ('Francesa', 'Francesa'),
-        ('Gaúcha', 'Gaúcha'),
-        ('Havaiana', 'Havaiana'),
-        ('Indiana', 'Indiana'),
-        ('Inglesa', 'Inglesa'),
         ('Italiana', 'Italiana'),
         ('Japonesa', 'Japonesa'),
-        ('Judaica', 'Judaica'),
         ('Mexicana', 'Mexicana'),
         ('Mineira', 'Mineira'),
-        ('Nordestina','Nordestina'),
-        ('Pernambucana', 'Pernambucana'),
-        ('Peruana', 'Peruana'),
-        ('Simples', 'Simples'),
-        ('Suíça', 'Suíça'),
-        ('Tailandesa', 'Tailandesa'),
         ('Vegetariana', 'Vegetariana'),
-        ('Natal', 'Natal'),
-        ('Rápidas', 'Rápidas'),
-        ('Frango', 'Frango'),
-        ('FrutosdoMar', 'FrutosdoMar'),
-        ('Lasanhas', 'Lasanhas'),
-        ('Legumes', 'Legumes'),
         ('Massas', 'Massas'),
         ('Molhos', 'Molhos'),
-        ('Peru', 'Peru'),
         ('Salada','Salada' ),
-        ('Salmão', 'Salmão'),
-        ('Sanduiches','Sanduiches'), 
-        ('Light', 'Light'),
-        ('Diet', 'Diet'),
-        ('Portuguesas', 'Portuguesas'),
-        ('-','-')
+        ('Light','Light' ),
     )
     nome = models.CharField(max_length=50)
-    tag = models.CharField(max_length=15, choices=TAGS, blank=False, null=False, default='-')
+    tag = models.CharField(max_length=15, choices=TAGS, blank=False, null=False, default='Doces')
     imagem = models.ImageField(blank=True)
     descricao = models.TextField(max_length=500)
     restaurante = models.ForeignKey(Restaurante, on_delete=CASCADE)
--- restaurantes/views.py
+++ restaurantes/views.py
@@ -7,7 +7,6 @@ from restaurantes.serializers import RestauranteSerializer, PratoSerializer, Lis
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 
-
 class RestauranteViewSet(viewsets.ModelViewSet):
     """Recurso de restaurantes"""
     queryset = Restaurante.objects.all()
@@ -24,6 +23,7 @@ class PratoViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'path', 'delete']
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome']
+    filterset_fields = ['tag']
 
 class ListaPratosDeUmRestauranteView(generics.ListAPIView):
     """Listando pratos de um restaurante"""
@@ -34,14 +34,72 @@ class ListaPratosDeUmRestauranteView(generics.ListAPIView):
 
 class ListandoTagsView(APIView):
     def get(self, request):
-        tags = {'tags': ['Aves', 'Arroz', 'Doces', 'Bolos', 'Caldos', 'Carne',
-                'Alemã', 'Americana', 'Árabe', 'Argentina', 'Australiana', 
-                'Baiana', 'Capixaba', 'Chinesa', 'Colombiana', 'Coreana', 
-                'Cubana', 'Espanhola', 'Francesa', 'Gaúcha', 'Havaiana', 
-                'Indiana', 'Inglesa', 'Italiana', 'Japonesa', 'Judaica', 
-                'Mexicana', 'Mineira', 'Nordestina', 'Pernambucana', 
-                'Peruana', 'Simples', 'Suíça', 'Tailandesa', 'Vegetariana', 
-                'Natal', 'Rápidas', 'Frango', 'Frutos do Mar', 'Lasanhas', 
-                'Legumes', 'Massas', 'Molhos', 'Peru', 'Salada', 'Salmão', 'Sanduiches',
-                'Light', 'Diet', 'Portuguesas', '-' ]}
+        tags = {
+        "tags": [
+            {
+            "value": "Italiana",
+            "color": "green"
+            },
+            {
+            "value": "Japonesa",
+            "color": "red"
+            },
+            {
+            "value": "Doces",
+            "color": "blue"
+            },
+            {
+            "value": "Diet",
+            "color": "yellow"
+            },
+            {
+            "value": "Massas",
+            "color": "indigo"
+            },
+            {
+            "value": "Caldos",
+            "color": "orange"
+            },
+            {
+            "value": "Light",
+            "color": "cyan"
+            },
+            {
+            "value": "Vegetariana",
+            "color": "sky"
+            },
+            {
+            "value": "Mexicana",
+            "color": "fuchsia"
+            },
+            {
+            "value": "Francesa",
+            "color": "pink"
+            },
+            {
+            "value": "Espanhola",
+            "color": "rose"
+            },
+            {
+            "value": "Mineira",
+            "color": "teal"
+            },
+            {
+            "value": "Baiana",
+            "color": "emerald"
+            },
+            {
+            "value": "Molhos",
+            "color": "lime"
+            },
+            {
+            "value": "Saladas",
+            "color": "amber"
+            },
+            {
+            "value": "Americana",
+            "color": "purple"
+            }
+        ]
+        }
         return JsonResponse(tags, status=status.HTTP_200_OK)
